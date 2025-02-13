ad263db4164af130599e5ff2a4b899c4211114b9
baylorjep <baylorjeppsen@gmail.com>
2025-02-12 17:59:10 -0700

    duplicate declaration removal

--- api/Autobid.js
+++ api/Autobid.js
@@ -1,5 +1,4 @@
 const openai = require("openai");
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const supabase = require("./supabaseClient"); 
 
 const generateAutoBidForBusiness = async (businessId, requestDetails) => {
